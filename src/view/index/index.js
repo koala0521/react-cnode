55ea08746df0456a2e23df2b6abd50d8f1c793e8
koala0521 <328762691@qq.com>
2018-03-04 23:58:24 +0800

    首页页面结构配置

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
-export default class User extends Component{
+import { Row , Col } from 'antd';
+
+import LeftMenu from '../leftMenu';
+import List from '../list';
+
+export default class IndexPage extends Component{
 
     render() {
+        console.log( this.props );      
+        let { id }  = this.props.match.params;
         return (
-            <div>
-                首页
+            <div className="wrap" >
+                <Row>
+                    <Col  md={ 6 } xs={ 0 } className="indexSider" >  
+                        <LeftMenu id="indexMenu" mode="vertical"  />
+                    </Col>
+                    
+                    <Col  md={ 0 } xs={ 24 } >  
+                    
+                        <LeftMenu id="indexXsMenu" mode="horizontal"  />
+                    </Col>
+
+                    <Col md={ 18 } xs={ 24 } className="indexList"  >
+                        <List id={ id } />
+                    </Col>
+                </Row>
             </div>
         );
     }
