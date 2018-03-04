962e1ed092c894eb330ae12032243bc2456a0dc0
koala0521 <328762691@qq.com>
2018-03-04 23:59:04 +0800

    content 内容列表

---
+++ src/view/list/index.js
@@ -0,0 +1,25 @@
+// import {  Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { List } from 'antd';  // antd 的列表组件  loading 布尔值  记录是否展示loading动画化组件
+
+// 列表假数据
+import listData from '../../data';
+
+export default class Item extends Component{
+
+    render(){
+        let {data} = listData;
+        console.log( data );
+        return (
+            <List 
+                // loading={ false } 
+                // dataSource={ data }
+                // renderItem = { (item)=>(<List.item>
+
+                // </List.item>)}
+            
+            >
+            </List>   
+        )
+    }
+}
