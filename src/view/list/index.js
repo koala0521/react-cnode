ef5b5d51c4d9aeecf9e00d7423d853bc2d746215
koala0521 <328762691@qq.com>
2018-03-06 00:10:27 +0800

    antd列表组件使用

--- src/view/list/index.js
+++ src/view/list/index.js
@@ -1,22 +1,48 @@
 // import {  Link } from 'react-router-dom';
 import React, { Component } from 'react';
-import { List } from 'antd';  // antd 的列表组件  loading 布尔值  记录是否展示loading动画化组件
+import { List , Avatar } from 'antd';  //  List :antd 的列表组件  loading 布尔值  记录是否展示loading动画化组件 ; Avatar : 列表头像组件
+import { Link } from 'react-router-dom';
 
 // 列表假数据
 import listData from '../../data';
+
+// 
+import Tag from '../txtTag';
 
 export default class Item extends Component{
 
     render(){
         let {data} = listData;
-        console.log( data );
         return (
             <List 
                 loading={ false } 
                 dataSource={ data }
-                renderItem = { (item)=>(<List.Item>
+                renderItem = { (item)=>(
+                    <List.Item
+                        key={ Math.random() }
+                        actions={[
+                            "回复" + item.reply_count , 
+                            "访问" + item.visit_count
+                        ]}
+                    >
+                    <List.Item.Meta
+                        avatar={ <Avatar src={ item.author.avatar_url } />}
+                        title={ 
+                            <Link to={ "/details/" + item.id } >                                                      
+                                <Tag  data={ item } />
+                                { item.title  }
+                            </Link>
+                        }
+                        description={ <p>      
+                            <Link to={ "/user/" + item.author.loginname} >    
+                                { item.author.loginname + " " }
+                            </Link>       
+                             发表于：{ item.create_at.split('T')[0] }     
+                        </p> }
+                    />
 
-                </List.Item>)}
+                    </List.Item>
+                )}
             
             >
             </List>   
