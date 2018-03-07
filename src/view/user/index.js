7da7a0688a2cd9471592ad9482fdf26a4ac6995c
unknown <gaopengfei@mvwchina.com>
2018-03-07 17:57:16 +0800

    用户信息页面

--- src/view/user/index.js
+++ src/view/user/index.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import { Avatar ,Row , Col } from 'antd';
+
+import UserList from './replyList';
+
+import data from './data';
 
 export default class User extends Component{
 
     render() {
+
+        let { avatar_url , loginname , create_at , score  } = data.data;
         return (
-            <div>
-                用户信息页面
+            <div className="wrap" >
+                <Avatar
+                    src={ data.data.avatar_url }
+                    className="userAvatar"
+                ></Avatar>
+                <Row
+                    className="userInfo"
+                >
+                    <Col span={8}> 用户名： <a> { loginname } </a></Col>
+                    <Col span={8}> 积分： <a> {score } </a></Col>
+                    <Col span={8}> 注册时间：<a> { create_at.split("T")[0] } </a></Col>
+                </Row>
+
+                <UserList 
+                    loading={ false }
+                    data={ data.data.recent_replies } 
+                    title="最近创建的话题"
+                
+                />
+
+                <UserList 
+                    loading = { false }
+                    data = { data.data.recent_replies }
+                    title="最近回复的话题"
+                    
+                />
+
             </div>
         );
     }
---
+++ src/view/user/replyList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Avatar ,List ,Card } from 'antd';
+import { Link } from "react-router-dom";
+import data from '../about/data';
+
+export default class ReplyList extends Component{
+
+    render() {
+        let { loading,title,data } = this.props;
+      return (
+        <Card
+            loading={ loading }        
+            title={ title }
+        >
+            <List
+                className="userList"
+                dataSource={ data }
+                renderItem={ item=>( 
+                    <List.Item>                                  
+                        <Avatar src={ item.author.author_url } />                   
+                        <Link to={"/user/" + item.author.loginname } >{ item.author.loginname }</Link>   
+                        <h4>
+                            <Link to={"/details/" + item.id} className = "userName" > { item.title } </Link>    
+                        </h4>
+                        <span> 最后回复时间:{ item.last_reply_at.split("T")[0] } </span>   
+                    </List.Item>                    
+                )}
+            >
+
+
+            </List>
+        </Card>
+      )
+    }
+
+}
