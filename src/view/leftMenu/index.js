7a93da640ef1dc373e1b713dc586e349324fff34
koala0521 <328762691@qq.com>
2018-03-15 23:46:43 +0800

    菜单配置数据抽象

--- src/view/leftMenu/index.js
+++ src/view/leftMenu/index.js
@@ -2,43 +2,8 @@ import {  Link } from 'react-router-dom';
 import { Menu  } from 'antd';
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-
-let tab = [
-    {
-        tab:"all",
-        txt: "全部",
-        isIndex: true
-    },{
-        tab: "top",
-        color: "magenta",
-        txt: "置顶",
-        isIndex: false
-    },{
-        tab:"good",
-        txt: "精华",
-        isIndex: true,
-        color: "geekblue"
-    },{
-        tab:"ask",
-        txt: "问题",
-        isIndex: true
-    },{
-        tab:"share",
-        txt: "分享",
-        isIndex: true,
-        color: "purple"
-    },{
-        tab:"job",
-        txt: "招聘",
-        isIndex: true,
-        color: "cyan"
-    },{
-        tab:"dev",
-        txt: "测试",
-        isIndex: true,
-        color: "lime"
-    }
-];
+// 菜单配置数据
+import tab from '../tab';
 
 class LeftMenu extends Component{
 
